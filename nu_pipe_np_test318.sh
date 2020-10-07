08398293958e781b614caff287cc2dffbd4927e1
Iain Hannah <iain.hannah@glasgow.ac.uk>
2020-10-08 00:57:42 +0100

    minor update

--- nu_pipe_np_test318.sh
+++ nu_pipe_np_test318.sh
@@ -7,20 +7,20 @@
 fulld="/Users/iain/data/heasarc_nustar/ns_20190425/20511318001/"
 id="nu20511318001"
 
-# Run it the "new" way with just default filtering
+# Run it the "new" way with just default status flags
 
 nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
 	outdir=event_cl_def entrystage=1 exitstage=2 pntra=OBJECT \
     pntdec=OBJECT hkevtexpr=NONE clobber=yes
 
-# Run it the "new" way with specific filtering (more for low livetime)
+# Run it the "new" way with specific flags (more for low livetime)
 
 nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
 	outdir=event_cl_nws entrystage=1 exitstage=2 pntra=OBJECT \
     pntdec=OBJECT statusexpr="STATUS==b0000xxx00xxxx000" \
     hkevtexpr=NONE clobber=yes
 
-# Run it the old way with specific filtering (more for low livetime)
+# Run it the old way with specific flags (more for low livetime)
 
 nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
 	outdir=event_cl_ods entrystage=1 exitstage=2 pntra=OBJECT \
