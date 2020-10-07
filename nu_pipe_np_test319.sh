8462e01bf3f5e76e9e61a0879f97535d7842ba4d
Iain Hannah <iain.hannah@glasgow.ac.uk>
2020-10-08 00:14:58 +0100

    initial example files

---
+++ nu_pipe_np_test319.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This is being run in indir
+
+# This is to check what to do with higher live time cases
+
+fulld="/Users/iain/data/heasarc_nustar/ns_20190425/20511319001/"
+id="nu20511319001"
+
+# Run it the "new" way with just default filtering
+
+nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
+	outdir=event_cl_def entrystage=1 exitstage=2 pntra=OBJECT \
+    pntdec=OBJECT hkevtexpr=NONE clobber=yes
+
+# Run it the "new" way with specific filtering (more for low livetime)
+
+nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
+	outdir=event_cl_nws entrystage=1 exitstage=2 pntra=OBJECT \
+    pntdec=OBJECT statusexpr="STATUS==b0000xxx00xxxx000" \
+    hkevtexpr=NONE clobber=yes
+
+# Run it the old way with specific filtering (more for low livetime)
+
+nupipeline obsmode=SCIENCE_SC indir=$fulld steminputs=$id \
+	outdir=event_cl_ods entrystage=1 exitstage=2 pntra=OBJECT \
+    pntdec=OBJECT statusexpr="STATUS==b0000xx00xx0xx000" cleanflick=no \
+    hkevtexpr=NONE clobber=yes
